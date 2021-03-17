58c405067231747e9babd7fb94aad1d4c518dab6
J-HLee <chrhflt1996@hanyang.ac.kr>
2021-03-17 19:07:54 +0900

    0317

--- myboard/src/main/java/tommy/spring/web/board/impl/BoardServiceImpl.java
+++ myboard/src/main/java/tommy/spring/web/board/impl/BoardServiceImpl.java
@@ -3,14 +3,13 @@ package tommy.spring.web.board.impl;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import tommy.spring.web.board.BoardService;
 import tommy.spring.web.board.BoardVO;
 
 @Service("boardService")
 public class BoardServiceImpl implements BoardService {
 	@Autowired
-	private BoardDAO boardDAO;
+	private BoardDAOSpring boardDAO;
 
 	public void insertBoard(BoardVO vo) {
 		if (vo.getSeq() == 0) {
